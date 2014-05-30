b52850b2765a89fd0e0d6fa9b6e0062acfe6f187
Sam Gross <colesbury@gmail.com>
2014-05-29 20:31:06 -0400

    Add getNamedGlobal and getFunctionList bindings.

--- src/module.cc
+++ src/module.cc
@@ -38,6 +38,13 @@ static Handle<Value> getOrInsertFunction(const Arguments& args){
 	return scope.Close(pFunction.create(fn, args.This(), args[1]));
 }
 
+static Handle<Value> getNamedGlobal(const Arguments& args){
+	ENTER_METHOD(pModule, 1);
+	STRING_ARG(name, 0);
+	llvm::GlobalVariable* global = self->getNamedGlobal(name);
+	return scope.Close(pGlobalVariable.create(global, args.This(), args[1]));
+}
+
 static Handle<Value> getOrInsertGlobal(const Arguments& args){
 	ENTER_METHOD(pModule, 2);
 	STRING_ARG(name, 0);
@@ -45,6 +52,21 @@ static Handle<Value> getOrInsertGlobal(const Arguments& args){
 	// TODO: should be llvm:Constant*
 	auto global = static_cast<llvm::GlobalVariable*>(self->getOrInsertGlobal(name, type));
 	return scope.Close(pGlobalVariable.create(global, args.This(), args[1]));
+}
+
+static Handle<Value> getFunctionList(const Arguments& args){
+	ENTER_METHOD(pModule, 0);
+
+	llvm::Module::FunctionListType& list = self->getFunctionList();
+	Handle<Array> array = Array::New(list.size());
+	unsigned idx = 0;
+    for (auto it = list.begin(), it_end = list.end(); it != it_end; ++it) {
+    	llvm::Function* fun = it;
+    	array->Set(idx, pFunction.create(fun, args.This()));
+    	++idx;
+    }
+
+	return scope.Close(array);
 }
 
 static Handle<Value> dump(const Arguments& args){
@@ -65,11 +87,11 @@ static void init(Handle<Object> target){
 	pModule.addMethod("getOrInsertFunction", &getOrInsertFunction);
 	pModule.addMethod("getFunction", &getFunction);
 
-//	pModule.addMethod("getNamedGlobal", &getNamedGlobal)
+	pModule.addMethod("getNamedGlobal", &getNamedGlobal);
 	pModule.addMethod("getOrInsertGlobal", &getOrInsertGlobal);
 
 //	pModule.addMethod("getGlobalList")
-//	pModule.addMethod("getFunctionList")
+	pModule.addMethod("getFunctionList", &getFunctionList);
 
 	pModule.addMethod("dump", &dump);
 }
